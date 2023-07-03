7b2c45c0898ad934d2d9774a9da5f045a1594da9
Florencia <florprados@gmail.com>
2023-07-03 20:35:42 -0300

    Agregar archivos de proyecto.

---
+++ Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace APIMongoDb.Models
+{
+    public class Product
+    {
+        [BsonId]
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+        public int Stock { get; set; }
+        public DateTime ExpiryDate { get; set; }
+        public string Category { get; set; }
+
+
+    }
+}
