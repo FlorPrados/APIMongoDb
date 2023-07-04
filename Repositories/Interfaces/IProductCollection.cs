cbd917db8ee9ea92649d4dfa1582223956fd9fb8
Florencia <florprados@gmail.com>
2023-07-04 14:45:23 -0300

    HTTP methods

---
+++ Repositories/Interfaces/IProductCollection.cs
@@ -0,0 +1,13 @@
+﻿using APIMongoDb.Models;
+
+namespace APIMongoDb.Repositories.Interfaces
+{
+    public interface IProductCollection
+    {
+        Task InsertProduct(Product product);
+        Task UpdateProduct(Product product);
+        Task DeleteProduct(string id);
+        Task<List<Product>> GetAllProducts();
+        Task<Product> GetProductById(string id);
+    }
+}
